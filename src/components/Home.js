1a5d4c94de1b6691355c099935fa39862e8052c8
Nwachukwu Kingsley Tochukwu <nwachukwu.kingsley01@yahoo.com>
2023-01-19 15:54:41 +0100

    set matches

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,52 @@
 import axios from "axios";
-import {useEffect, useCallback} from 'react';
+import { useEffect, useCallback, useState } from "react";
 
 const Home = () => {
+  const [matches, setMatches] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const callBck = useCallback(async () => {
+    try {
+      setLoading(true);
+      setErrorMessage("");
+      const res = await axios.get(
+        "https://api.football-data.org/v2/teams/67/matches?status=SCHEDULED&limit=1",
+        {
+          headers: {
+            "X-Auth-Token": "24b6711002744c65a971e59d3f9f36b1",
+          },
+        }
+      );
+      setMatches(res.data.matches);
+    } catch (err) {
+      setErrorMessage(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
 
-const callBck=useCallback(async () =>{
+  useEffect(() => {
+    callBck();
+  }, [callBck]);
 
-
-const options = {
-  method: 'GET',
-  url: 'https://api.football-data.org/v2/teams/67/matches?status=SCHEDULED&limit=1',
-  headers: {
-    'X-Auth-Token':'24b6711002744c65a971e59d3f9f36b1'
+  if (loading) {
+    return <h1>Loading...</h1>;
   }
+
+  if (errorMessage) {
+    return <h1>{errorMessage}</h1>;
+  }
+  return (
+    <div>
+      <ul>
+        {matches.map((match) => (
+          <li key={match.id}>
+            {match.homeTeam.name} vs {match.awayTeam.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-},[])
-
-// useEffect(()=>{
-//  callBck();
-// },[callBck]);
-
-
-return(
-	
-<button onClick={e=>{
-	e.preventDefault()
-	callBck();
-}}>hy</button>
-	);
-}
 
 export default Home;
